9dcc8052a7ee65789b610e71293ffb7dbbc9e7b6
djedd1ne <58569774+djedd1ne@users.noreply.github.com>
2025-01-13 23:32:40 +0100

    Ask user for input to validate close matches

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ def translate (word):
     if word in data:
         return data[word]
     elif len(get_close_matches(word, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
+        yn = input("Did you mean %s instead?, Enter Y for Yes or N for No: " % get_close_matches(word, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
 
     else:
         return "The word doesn't exist."
